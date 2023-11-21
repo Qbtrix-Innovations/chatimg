56b1e3f402fab5b33c527767fc2810618c4da6fe
Akash <akashkulkarni4444@gmail.com>
2023-11-21 16:18:17 +0530

    did the route names changes ?

--- src/routes/Chats/[chatId]/+page.server.js
+++
@@ -1,7 +0,0 @@
-import { error } from '@sveltejs/kit';
-/** @type {import('./$types').PageServerLoad} */export async function load({ params }) {
-    // const chat = await getChatfromDatabase();
-    // const post = await getPostFromDatabase(params.chatId);
-    // if (post) { return post; }
-    // throw error(404, 'Not found');
-}
