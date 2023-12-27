04ca681081f71c02a4e91b6e66f3e23ad41b109f
Akash <akashkulkarni4444@gmail.com>
2023-12-27 10:08:30 +0530

    Google Oauth added.

--- src/routes/auth/components/ExternalLoginButton.svelte
+++ src/routes/auth/components/ExternalLoginButton.svelte
@@ -13,10 +13,14 @@
 	 */
     let className = '';
 	export { className as class };
-    // export handleAuthClick ;
+    /**
+	 * @type {() => void}
+	 */
+     export let handleAuthClick ;
 </script>
 
 <Button
+    on:click={()=>{handleAuthClick()}}
     class={`flex flex-row justify-start items-center border-[0.5px] rounded-[6px] h-7 w-full bg-[rgba(227,227,227,1)] bg-gradient-to-b from-[rgba(255,255,255,1)] to-[rgba(185,185,185,0.39)] ${className}`}>
     <div class="text-[14px] leading-5">    
         {@html icn}
